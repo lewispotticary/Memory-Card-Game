a5cf2adcfa5ed41a070090cd90aeccb9a103c83b
Lewis Potticary <lewispotticary@gmail.com>
2021-06-29 22:45:54 +0100

    Created img elements and storing html elements

---
+++ script.js
@@ -0,0 +1,19 @@
+var blackCat = document.createElement("img");
+blackCat.src = "img/blackCat.jpg";
+
+var greyCat = document.createElement("img");
+greyCat.src = "img/greyCat.jpg";
+
+var orangeCat = document.createElement("img");
+orangeCat.src = "img/orangeCat.jpg";
+
+
+var card1 = document.getElementsByClassName("card1")[0];
+var card2 = document.getElementsByClassName("card2")[0];
+var card3 = document.getElementsByClassName("card3")[0];
+var card4 = document.getElementsByClassName("card4")[0];
+var card5 = document.getElementsByClassName("card5")[0];
+var card6 = document.getElementsByClassName("card6")[0];
+
+//card1.appendChild(blackCat);
+//random int and decrease 
