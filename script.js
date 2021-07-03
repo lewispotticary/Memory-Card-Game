c6cf906c2855ddacc0f2d90ef7c2a398d2e124b2
Lewis Potticary <lewispotticary@gmail.com>
2021-07-03 17:48:40 +0100

    Append images in random order to cards

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-var blackCat = document.createElement("img");
-blackCat.src = "img/blackCat.jpg";
+var numberArr = [1,2,3,4,5,6];
 
-var greyCat = document.createElement("img");
-greyCat.src = "img/greyCat.jpg";
+for(var x=1; x < 7; x++){
+    var num = Math.floor(Math.random() * numberArr.length);
+    var roll = numberArr.splice(num, 1);
+    var finalNum = roll[ 0 ];
+    console.log("Final Number:" + finalNum);
+    var cardTemp = document.getElementsByClassName("card" + finalNum)[0];
+    console.log(cardTemp);
+    var catTemp = document.getElementById(x);
+    console.log(catTemp);
+    cardTemp.appendChild(catTemp);
+}
 
-var orangeCat = document.createElement("img");
-orangeCat.src = "img/orangeCat.jpg";
-
-
-var card1 = document.getElementsByClassName("card1")[0];
-var card2 = document.getElementsByClassName("card2")[0];
-var card3 = document.getElementsByClassName("card3")[0];
-var card4 = document.getElementsByClassName("card4")[0];
-var card5 = document.getElementsByClassName("card5")[0];
-var card6 = document.getElementsByClassName("card6")[0];
-
-//card1.appendChild(blackCat);
-//random int and decrease 
